8c01f8f1739b8da59795f69a066536bca4735550
EvilStorm <gevilstorm@gmail.com>
2015-12-30 10:06:53 +0900

    DB 연결 테스트

--- db_creator.php
+++ db_creator.php
@@ -8,11 +8,14 @@
   	
   	$conn = mysql_connect($db_host, $db_user, $db_password);
 
-	echo " CONNECT : " . $conn;
+	if($conn){
+		echo " DB CONNECT 성공 " ;
+	}else{
+		echo " DB CONNECT : 실패 ";
+	}
+
 
 	$result = mysql_select_db($db_name, $conn);
-
-	echo " CONNECT RESULT : " . $result;
 
 agreement();
 notice();
